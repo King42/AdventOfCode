24399c4effdceaca991c7c7ee6d712bb8f631a38
Winston King <winston.king@mouser.com>
2024-12-04 12:30:29 -0600

    Day 4 setup

---
+++ src/AdventOfCode2024/Day4/Solver.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day4;
+
+public class Solver : SolverBase
+{
+    public Solver(int day, bool debug, bool useTestData)
+        : base(day, debug, useTestData)
+    {
+    }
+
+    public override (object? part1, object? part2) Solve()
+    {
+        return ("NYI", "NYI");
+    }
+}
