dd8218de3368c1fd7ba00dbc6e94764bf3004028
Winston King <winston.king@mouser.com>
2024-12-10 09:54:57 -0600

    Day 10 setup

---
+++ src/AdventOfCode2024/Day10/Solver.cs
@@ -0,0 +1,18 @@
+using AdventOfCodeCore;
+
+namespace AdventOfCode2024.Day10;
+
+public class Solver : SolverBase
+{
+    protected override bool UseTestData => true;
+    protected override bool Debug => true;
+
+    public Solver(int day) : base(day)
+    {
+    }
+
+    public override (object? part1, object? part2) Solve()
+    {
+        return ("NYI", "NYI");
+    }
+}
